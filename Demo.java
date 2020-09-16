69834c45a42165595730f70d88fa57ba02e7afb0
FelixvL <digitaleservice@gmail.com>
2020-09-16 11:58:19 +0200

    CB

--- Demo.java
+++ Demo.java
@@ -1,11 +1,13 @@
-class Demo {  // CA
+class Demo {  // CB
 	public static void main(String... args){
-		Arts karel = new Arts();
+		Arts karel = new Chirurg();
 		karel.BelleBellen();
 	}
 }
 abstract class Arts{
-
+	void BelleBellen(){
+		System.out.println("Wat is je naam?");
+	}
 }
 class Chirurg extends Arts{
 	void BelleBellen(){
