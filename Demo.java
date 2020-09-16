93a40fa052c1ae5e7040173e00c7229823dc763c
FelixvL <digitaleservice@gmail.com>
2020-09-16 11:56:11 +0200

    CA

--- Demo.java
+++ Demo.java
@@ -1,14 +1,18 @@
-class Demo {  // BK
+class Demo {  // CA
 	public static void main(String... args){
-		int[][][] go = { { {5,6,2,8,9}}, {{4,5,6,7},{4,25}},{{8,7,9} }     };
+		Arts karel = new Arts();
+		karel.BelleBellen();
+	}
+}
+abstract class Arts{
 
-		int[]t = go[1][2];
-		System.out.println(t);
-		
+}
+class Chirurg extends Arts{
+	void BelleBellen(){
+		System.out.println("Hoe gaat het?");
 	}
 
 }
-
 
 
 
