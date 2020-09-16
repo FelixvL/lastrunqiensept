39cf965dd7357b0ba787498aaa7512bb9c2d611b
FelixvL <digitaleservice@gmail.com>
2020-09-16 14:01:16 +0200

    DO

--- Demo.java
+++ Demo.java
@@ -1,40 +1,39 @@
-class Demo {  // DN
+class Demo {  // DO
 	public static void main(String... args){
-		Wasmachine wm = new Wasmachine();
-		wm.wassen(new Broek());
-		wm.wassen(new Blouse());
+		Wasmachine wm1 = new Wasmachine();
+		Wasmachine wm2 = new Wasmachine();
+		wm2.wassen(null);
+		wm1.wassen(new Broek());
+		System.out.println(wm1.gewicht);
 	}
 }
-
 class Wasmachine{
+	static int gewicht = optillen();
 	void wassen(Kleding w){
+		if(w == null){
+			gewicht = 10;
+			return;
+		}
 		System.out.println("wasjes" + w.temperatuur);
 		w.wassen();
 	}
-	
+	int optillen(){
+		System.out.println("aan het optillen");
+		return 9;
+	}	
 }
-
-class Broek extends Kleding implements SpijkerStof{
+class Broek extends Kleding{
 	int temperatuur = 4;
 }
-
-class ZitPoef implements SpijkerStof{
-	int temperatuur = 5;
-}
-
 class Kleding implements Wasbaar{
 	int temperatuur = 6;
 	public void wassen(){
 		System.out.println("Wassen in Kleding"+ temperatuur);
 	}
 }
-
 interface Wasbaar{
 	void wassen();
 }
-
-interface SpijkerStof{}
-
 class Blouse extends Kleding{
 	int temperatuur = 7;
 	public void wassen(){
