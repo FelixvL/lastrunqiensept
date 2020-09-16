4fbd3152b8f714abe9b4ed702fdecad2ce732b31
FelixvL <digitaleservice@gmail.com>
2020-09-16 16:12:06 +0200

    HC

--- Demo.java
+++ Demo.java
@@ -1,43 +1,14 @@
-public class Demo {   // HB
-    public static void main(String[] args) {
-        System.out.println("even een leuke dier vinden");
-        Dier nina = new Hond();
-        Beer yogi = new Beer();
-        Dier felix = new Mens();
-        felix.spelen(nina, yogi);
-    }
+class Lion{   // HC
+    Lion l = new Lion();
+
+    static void public main(String[]args){
+new Lion();
 }
 
-interface Huisdier {
-    default void chillen() {
-        System.out.println("de huisdier is relaxed");
-    }
+public void roar(){
+Lion l = new Lion();
+if (l==l){
+System.out.println("roar!");
 }
-abstract class Dier {
-    abstract void aaien() throws Exception;
-    public void spelen(Dier een, Dier twee) {
-        try {
-            twee.aaien();
-        } catch (Exception e) {
-            System.out.println( "DE BEER IS BOOS! WEG LOPEN!");
-        }
-    }
 }
-class Mens extends Dier {
-    void aaien() {
-        System.out.println("dit is heel raar");
-    }
-}
-class Hond extends Dier implements Huisdier {
-    public void aaien() {
-        System.out.println("wat een geweldig beest");
-    }
-    public void chillen() {
-        System.out.println("de hond is chill");
-    }
-}
-class Beer extends Dier {
-    public void aaien() throws Exception {
-        throw new Exception();
-    }
 }
