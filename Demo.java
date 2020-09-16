dbe37286a93ae05893083f693e6418c6bbd1c5e9
FelixvL <digitaleservice@gmail.com>
2020-09-16 11:45:00 +0200

    BK

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
-class Demo {  // BF
+class Demo {  // BK
 	public static void main(String... args){
 		int[][][] go = { { {5,6,2,8,9}}, {{4,5,6,7},{4,25}},{{8,7,9} }     };
 
-		int t = go[0][0];
+		int[]t = go[1][2];
 		System.out.println(t);
 		
 	}
