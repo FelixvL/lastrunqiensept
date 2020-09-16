18e4065ae24f6f13a5fad1e7b11389ac00f986af
FelixvL <digitaleservice@gmail.com>
2020-09-16 11:22:35 +0200

    BE

--- Demo.java
+++ Demo.java
@@ -1,10 +1,11 @@
-class Demo {  // BC
+class Demo {  // BE
 	public static void main(String... args){
 		int[][][] go = {    { {5,6,2,8,9}}, {{4,5,6,7},{4,25}},{{8,7,9} }     };
 
-		for(int a = 0; a < go[0].length; a++){
-			for(int b = 0; b < go[0][a].length; b++){
-				System.out.println(">>"+go[0][a][b]);
+		for(int a = 0; a < go[1].length; a++){
+			System.out.println("Boe");
+			for(int b = 0; b < go[1][a].length; b++){
+				System.out.println(">>"+go[1][a][b]);
 			}
 		}
 
