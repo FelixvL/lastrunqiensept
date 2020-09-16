751b6629d167e38e3ec9caf8635aebd15f02864f
FelixvL <digitaleservice@gmail.com>
2020-09-16 12:09:32 +0200

    CH

--- Demo.java
+++ Demo.java
@@ -1,17 +1,17 @@
-class Demo {  // CG
+class Demo {  // CH
 	public static void main(String... args){
 		System.out.println("Hij doet het");
 	}
 }
 abstract class Arts{
-	abstract void BelleBellen();
+	abstract protected void BelleBellen();
 }
 abstract class Chirurg extends Arts{
 
 
 }
 class KaakChirurg extends Chirurg{
-	protected void BelleBellen(){
+	void BelleBellen(){
 		System.out.println("Heb je goed gepoetst?");
 	}
 
