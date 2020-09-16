c13dc66ccbd65c45795933bf28499b76be940316
FelixvL <digitaleservice@gmail.com>
2020-09-16 10:52:01 +0200

    AL

--- Demo.java
+++ Demo.java
@@ -1,7 +1,10 @@
-class Demo {  // AK  // Ik wil de acht op het scherm
+class Demo {  // AL
 	public static void main(String... args){
 		int[][][] go = {null, null, {null, {4,5,6,7},{4,5}},{{8,7,6}, null }};
-		System.out.println("FF checken"+go);
+
+
+
+		System.out.println("FF checken "+go[2][1].length);
 
 	}
 
