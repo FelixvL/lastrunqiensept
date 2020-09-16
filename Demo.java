bab6d833d544e8a95983ca2cb8ce5da70e33c762
FelixvL <digitaleservice@gmail.com>
2020-09-16 13:05:15 +0200

    DA

--- Demo.java
+++ Demo.java
@@ -1,29 +1,37 @@
-class Demo {  // CI
+class Demo {  // DA
 	public static void main(String... args){
-		System.out.println("Hij doet het");
-		KaakChirurg kees = new KaakChirurg();
-		kees.BelleBellen(25);
-
+		SpijkerStof sp = new Broek();
+		Wasmachine w = new Wasmachine();
+		w.wassen(sp);
+		System.out.println("Einde");
 	}
 }
-abstract class Arts{
-	abstract void BelleBellen();
-}
-abstract class Chirurg extends Arts{
-
-
-}
-class KaakChirurg extends Chirurg{
-	int BelleBellen(int a){
-		System.out.println("Heb je goed gepoetst?");
-		return 5;
+class Wasmachine{
+	void wassen(Wasbaar w){
+		System.out.println("wasjes");
 	}
+	
+}
+
+class Broek extends Kleding implements SpijkerStof{
 
 }
 
+class Kleding implement Wasbaar{
 
+}
 
+interface Wasbaar{
 
+}
+
+interface SpijkerStof{
+
+}
+
+class Blouse implements Kleding{
+
+}
 
 
 
