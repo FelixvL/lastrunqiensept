8bc1099cc95f0b5ad3579b645e8adcf88afae4e1
FelixvL <digitaleservice@gmail.com>
2020-09-16 15:12:40 +0200

    EL

--- Demo.java
+++ Demo.java
@@ -1,9 +1,12 @@
-class Demo {  // EI
+import java.io.*;
+
+class Demo {  // EL
 	public static void main(String... args){
 		System.out.println("baantje trekken");
 		Zwembad tongelreep = new Zwembad();
 		tongelreep.aantalCM = 35;
 		Zwemmer pieter = new Zwemmer();
+		pieter.z = tongelreep;
 		try{
 			pieter.zwemmen();
 		}catch(Throwable e){
@@ -14,19 +17,21 @@ class Demo {  // EI
 
 class Zwemmer{
 	Zwembad z = new Zwembad();
-	void zwemmen() throws Throwable{
+	void zwemmen() throws Exception, IOException{
 		if(z.aantalCM < 50){
-			try{
-				throw new Throwable();
-			}catch(Exception t){
-				System.out.println("FF de kraan aanzetten");
-			}
+
+				throw new IOException();
+
 		}else{
 			System.out.println("FF mijn brilletje opzetten");
 		}
 	}
 }
-
+class KanaalZwemmer extends Zwemmer{
+	void zwemmen() throws Exception{
+		System.out.println("Ik wil ook op Brexit");
+	}
+}
 class Zwembad{
 	int aantalCM = 50;
 }
