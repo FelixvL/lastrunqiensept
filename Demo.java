a6426847a7f37bc358f6fc1af3673f7621e61e9f
FelixvL <digitaleservice@gmail.com>
2020-09-16 14:37:54 +0200

    EA

--- Demo.java
+++ Demo.java
@@ -1,46 +1,23 @@
-class Demo {  // DQ
+class Demo {  // EA
 	public static void main(String... args){
-		Wasmachine wm1 = new Wasmachine();
-		Wasmachine wm2 = new Wasmachine();
-		wm2.wassen(null);
-		wm1.wassen(new Broek());
-		System.out.println(wm1.gewicht);
-	}
-}
-class Wasmachine{
-	static int gewicht = optillen();
-	void wassen(Kleding w){
-		if(w == null){
-			gewicht = 10;
-			return;
-		}
-		System.out.println("wasjes" + w.temperatuur);
-		w.wassen();
-	}
-	static int optillen(){
-		System.out.println("aan het optillen");
-		return 9;
-	}	
-}
-class Broek extends Kleding{
-	int temperatuur = 4;
-}
-class Kleding implements Wasbaar{
-	int temperatuur = 6;
-	public void wassen(){
-		System.out.println("Wassen in Kleding"+ temperatuur);
-	}
-}
-interface Wasbaar{
-	void wassen();
-}
-class Blouse extends Kleding{
-	int temperatuur = 7;
-	public void wassen(){
-		System.out.println("Blouse"+temperatuur);
+		System.out.println("baantje trekken");
 	}
 }
 
+class Zwemmer{
+	Zwembad z = new Zwembad();
+	void zwemmen(){
+		if(z.aantalCM < 50){
+			throw new Error();
+		}else{
+			System.out.println("FF mijn brilletje opzetten");
+		}
+	}
+}
+
+class Zwembad{
+	int aantalCM = 50;
+}
 
 
 
