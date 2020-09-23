33059c2160f5ed5028f9ff419afc6514e56b5f12
FelixvL <digitaleservice@gmail.com>
2020-09-23 14:54:07 +0200

    XF

--- Demo.java
+++ Demo.java
@@ -1,32 +1,34 @@
-import java.io.*;
-// XD
+// XF
 class Demo{
 	public static void main(String[] args){
 		RDW rdw = new RDW();
 		Auto waggie = new SportAuto();
-		Integer l = rdw.rijexamen(waggie);
-		System.out.println("ready"+l);
+		rdw.rijexamen(waggie);
+		rdw.rijexamen(new Auto());
+		System.out.println("ready");
 	}
 }
 class RDW{
 	void rijexamen(Auto sa){
 		try{
-			sa.rijden();
-		}catch(Exception e){
+			int getal = sa.rijden();
+		}catch(LekkeBandException e){
 			System.out.println("opgelost");
 		}
+		System.out.println(getal);
 	}
 }
 class Auto{
-	int rijden() throws RuntimeException{
+	int rijden() throws LekkeBandException {
 		System.out.println("riA");
 		return 7;
 	}
 }
 class SportAuto extends Auto{
-	int rijden() throws IOException{
+	int rijden() throws LekkeBandException{
 		System.out.println("riSA");
 		return 9;
 	}
 
 }
+class LekkeBandException extends Exception{}
