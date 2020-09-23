f3b676c3bb192451dce397878dd35ee473c81c9e
FelixvL <digitaleservice@gmail.com>
2020-09-23 13:27:04 +0200

    YC

--- Demo.java
+++ Demo.java
@@ -1,18 +1,16 @@
-
-// YZ
+// YC
 class Demo{
 	public static void main(String[] args){
 		System.out.println("Start");
 		Server s = new Server();
 		System.out.println(s.app);
-		App a = new iApp();
+		iApp a = new iApp("25");
 		s.app = a;
 		System.out.println(s.app.prijs);
 	}
 }
-
 class Server{
-	iApp app;
+	iApp app = new iApp("55");
 	App install(){
 		System.out.println("a");
 		return new iApp();
@@ -21,7 +19,15 @@ class Server{
 
 class App{
 	int prijs = 12;
+	App(int prijs){
+		System.out.println("hup app");
+		prijs = prijs;
+	}
 }
 class iApp extends App{
 	int prijs = 24;
+	iApp(int prijs){
+		System.out.println("hup iapp");
+		prijs = prijs;
+	}
 }
