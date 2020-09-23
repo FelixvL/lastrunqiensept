9044c459ee511f250215a607dda540b1a3503d6d
FelixvL <digitaleservice@gmail.com>
2020-09-23 15:31:41 +0200

    VH

--- Demo.java
+++ Demo.java
@@ -1,25 +1,16 @@
-// VF
+// VH
 class Demo{
 	public static void main(String[] args){
 		Meedoen m = x -> x * 3;
 		new Irr().gaan(m);
 	}
 }
-
 interface Meedoen{
 	default void ikdoemee(){
 		System.out.println("in Meedoen");
 	}
-	int doen(int getal);
+	int doen();
 }
-
-class Speler implements Meedoen{
-	public int doen(int naam){
-		System.out.println("Speler"+naam);
-		return 7;
-	}
-}
-
 class Irr {
 	void gaan(Meedoen r){
 		System.out.println(r.doen(5));
