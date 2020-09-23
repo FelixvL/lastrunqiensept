3da28b183bd73581e563339e70aa9efe5f70e934
FelixvL <digitaleservice@gmail.com>
2020-09-23 13:07:14 +0200

    reset

--- Demo.java
+++
@@ -1,7 +0,0 @@
-class Demo {  // HD  
-public static void main(String[ ] args){       
-    int[] a = { 1, 2, 3, 4 };       
-    int[] b = { 2, 3, 1, 0 };       
-System.out.println( a [ (a = b)[3] ] );    
-} 
-}
