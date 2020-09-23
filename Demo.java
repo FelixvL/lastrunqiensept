10ed5d0cfc73d40348bd6c1ee23b1a7fd57d7474
FelixvL <digitaleservice@gmail.com>
2020-09-23 13:37:50 +0200

    YJ

--- Demo.java
+++ Demo.java
@@ -1,16 +1,16 @@
-// YG
+// YJ
 class Demo{
 	public static void main(String[] args){
 		System.out.println("Start");
 		Server s = new Server();
 		System.out.println(s.app);
-		iApp a = new iApp(25);
+		App a = new App(25);
 		s.app = a;
 		System.out.println(s.app.prijs);
 	}
 }
 class Server{
-	iApp app = new iApp(55);
+	App app = new iApp(55);
 	App install(){
 		System.out.println("a");
 		return new iApp(23);
@@ -20,14 +20,14 @@ class Server{
 class App{
 	int prijs = 12;
 	App(int prijs){
-		System.out.println("hup app");
+		System.out.println("hup app"+prijs);
 		this.prijs = prijs;
 	}
 }
 class iApp extends App{
 	iApp(int prijs){
 		super(14);
-		System.out.println("hup iapp");
-		prijs = prijs;
+		System.out.println("hup iapp"+prijs);
+		this.prijs = prijs;
 	}
 }
